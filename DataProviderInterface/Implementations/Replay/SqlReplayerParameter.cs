2165dd3536767011058afb3cbc2db6df7eba5ad5
ProductiveRage <dangger36@gmail.com>
2016-05-18 19:15:51 +0100

    Ensure that the magic-but-useful default behaviour of the SqlParameter is precisely mimicked by the SqlReplayerParameter

--- DataProviderInterface/Implementations/Replay/SqlReplayerParameter.cs
+++ DataProviderInterface/Implementations/Replay/SqlReplayerParameter.cs
@@ -1,52 +1,67 @@
 ﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace ProductiveRage.SqlProxyAndReplay.DataProviderInterface.Implementations.Replay
 {
 	public sealed class SqlReplayerParameter : IDbDataParameter
 	{
-		private object _value;
+		private readonly SqlParameter _parameter;
 		public SqlReplayerParameter()
 		{
-			// Apply some sensible defaults - important for properties whose default state is invalid (eg. there is no zero value for the ParameterDirection
-			// enum) but more important that the property defaults here match those on SqlParameter for when the replayer aspect of the services comes to
-			// look in its cache (it will check that the connection, command and parameters are consistent between the current request and any cache entry,
-			// the direction of the parameter - Input, Output, InputOutput, ReturnValue - is important)
-			Direction = ParameterDirection.Input;
-			_value = null;
+			// The SqlParameter has all kinds of not-immediately-obvious behaviour that I don't want to replicate here, so I'm just going to
+			// use one behind the scenes. For example, if nothing is set other than ParameterName and Value and the Value is a string then
+			// an appropriate Size will be set on the parameter.
+			_parameter = new SqlParameter();
 		}
 
-		public DbType DbType { get; set; }
-
-		public ParameterDirection Direction { get; set; }
-
-		public bool IsNullable { get; set; } // TODO: Is this correct? What should set this value (it's read-only in the interface)?
-
-		public string ParameterName { get; set; }
-
-		public byte Precision { get; set; }
-
-		public byte Scale { get; set; }
-
-		public int Size { get; set; }
-
-		public string SourceColumn { get; set; }
-
-		public DataRowVersion SourceVersion { get; set; }
-
+		public DbType DbType
+		{
+			get { return _parameter.DbType; }
+			set { _parameter.DbType = value; }
+		}
+		public ParameterDirection Direction
+		{
+			get { return _parameter.Direction; }
+			set { _parameter.Direction = value; }
+		}
+		public bool IsNullable
+		{
+			get { return _parameter.IsNullable; }
+		}
+		public string ParameterName
+		{
+			get { return _parameter.ParameterName; }
+			set { _parameter.ParameterName = value; }
+		}
+		public byte Precision
+		{
+			get { return _parameter.Precision; }
+			set { _parameter.Precision = value; }
+		}
+		public byte Scale
+		{
+			get { return _parameter.Scale; }
+			set { _parameter.Scale = value; }
+		}
+		public int Size
+		{
+			get { return _parameter.Size; }
+			set { _parameter.Size = value; }
+		}
+		public string SourceColumn
+		{
+			get { return _parameter.SourceColumn; }
+			set { _parameter.SourceColumn = value; }
+		}
+		public DataRowVersion SourceVersion
+		{
+			get { return _parameter.SourceVersion; }
+			set { _parameter.SourceVersion = value; }
+		}
 		public object Value
 		{
-			get { return _value; }
-			set
-			{
-				var stringValue = value as string;
-				if (stringValue != null)
-				{
-					DbType = DbType.String; // TODO: Correct?
-					if (Size < stringValue.Length)
-						 Size = stringValue.Length; // TODO: Correct?
-				}
-				_value = value;
-			}
+			get { return _parameter.Value; }
+			set { _parameter.Value = value; }
 		}
 	}
 }
